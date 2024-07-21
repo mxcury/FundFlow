085717f18cde1a3520d1016fa120599fe822e086
Tom Flattery <flatterythomas48@gmail.com>
2024-07-21 15:13:00 +0100

    INITIAL: Initial commit with basic next.js file structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# FundFlow
+
+FundFlow is a simple app designed to link bank and current accounts and design automated tasks based on value and timed "jobs" to move money across and maximize on savings!
---
+++ fund-flow/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"], weight: ['400', '700'], variable: '--font-ibm-plex-serif' });
+
+export const metadata: Metadata = {
+  title: "FundFlow",
+  description: "FundFlow is a digital investment management platform for automated transfers between bank accounts.",
+  icons: { icon: '/icons/logo.svg'}
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
