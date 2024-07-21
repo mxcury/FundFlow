a4ce570dd6e490a2e232a7d0c6d08be2e5eeed88
Tom Flattery <flatterythomas48@gmail.com>
2024-07-21 22:17:42 +0100

    FEATURE: redesign logo to use FundFlow custom logo and add HeaderBox for pages ADDITION: add .gitignore file to exclude .vscode files

--- fund-flow/src/app/(root)/page.tsx
+++ fund-flow/src/app/(root)/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import HeaderBox from "@/components/ui/HeaderBox";
 
 const Home = () => {
-	return <div>Home</div>;
+	const loggedIn = { firstName: "John", lastName: "Doe" };
+
+	return (
+		<section className="home">
+			<div className="home-content">
+				<header className="home-header">
+					<HeaderBox
+						type="greeting"
+						title="Welcome"
+						user={loggedIn?.firstName || "Guest"}
+						subtext="Access and manage your automated transactions between accounts effciently"
+					></HeaderBox>
+				</header>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
--- fund-flow/src/app/layout.tsx
+++ fund-flow/src/app/layout.tsx
@@ -2,23 +2,30 @@ import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
-const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"], weight: ['400', '700'], variable: '--font-ibm-plex-serif' });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-ibm-plex-serif",
+});
 
 export const metadata: Metadata = {
-  title: "FundFlow",
-  description: "FundFlow is a digital investment management platform for automated transfers between bank accounts.",
-  icons: { icon: '/icons/logo.svg'}
+	title: "FundFlow",
+	description:
+		"FundFlow is a digital investment management platform for automated transfers between bank accounts.",
+	icons: { icon: "/icons/logo.svg" },
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
+				{children}
+			</body>
+		</html>
+	);
 }
