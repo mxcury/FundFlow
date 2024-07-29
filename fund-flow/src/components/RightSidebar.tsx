45cd54043516a3a6f87b2da83a6b28379aeeef76
Tom Flattery <flatterythomas48@gmail.com>
2024-07-29 12:54:12 +0100

    FEATURE: add pagination to transaction history page

--- fund-flow/src/components/RightSidebar.tsx
+++ fund-flow/src/components/RightSidebar.tsx
@@ -2,27 +2,32 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import BankCard from "./BankCard";
+import { countTransactionCategories } from "@/lib/utils";
+import Category from "./Category";
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
+	const categories: CategoryCount[] = countTransactionCategories(transactions);
+
 	return (
 		<aside className="right-sidebar">
 			<section className="flex flex-col pb-8">
-				<div className="profile-banner"></div>
+				<div className="profile-banner" />
 				<div className="profile">
 					<div className="profile-img">
 						<span className="text-5xl font-bold text-blue-500">
 							{user.firstName[0]}
 						</span>
 					</div>
+
 					<div className="profile-details">
 						<h1 className="profile-name">
-							{user.firstName}
-							{user.lastName}
+							{user.firstName} {user.lastName}
 						</h1>
 						<p className="profile-email">{user.email}</p>
 					</div>
 				</div>
 			</section>
+
 			<section className="banks">
 				<div className="flex w-full justify-between">
 					<h2 className="header-2">My Banks</h2>
@@ -39,6 +44,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
 						<h2 className="text-14 font-semibold text-gray-600">Add Bank</h2>
 					</Link>
 				</div>
+
 				{banks?.length > 0 && (
 					<div className="relative flex flex-1 flex-col items-center justify-center gap-5">
 						<div className="relative z-10">
@@ -61,6 +67,19 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
 						)}
 					</div>
 				)}
+
+				<div className="mt-10 flex flex-1 flex-col gap-6">
+					<h2 className="header-2">Top categories</h2>
+
+					<div className="space-y-5">
+						{categories.map((category, index) => (
+							<Category
+								key={category.name}
+								category={category}
+							/>
+						))}
+					</div>
+				</div>
 			</section>
 		</aside>
 	);
